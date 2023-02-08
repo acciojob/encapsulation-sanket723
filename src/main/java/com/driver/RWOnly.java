85a3686d4221eb4f5e8cc3bdbdba95c882216c18
sanket723 <sanketpawar723@gmail.com>
2023-02-08 17:23:52 +0530

    first attempt encapsulation assignment

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+     private String name;
+
+     public String getter(){
+          return name;
+     }
+     public String setter(String x){
+         return this.name = x;
+     }
+
 }
